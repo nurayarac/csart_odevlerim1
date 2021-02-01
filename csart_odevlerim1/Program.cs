0c4134ca6d0a972fe298935cb65b8bb33573a959
Nuray Araç <l2011012051@ogr.sdu.edu.tr>
2021-02-01 16:08:10 +0300

    Add project files.

---
+++ csart_odevlerim1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace csart_odevlerim1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mobilyalar = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "tv ünitesi";
+            urun1.Fiyati = 600;
+            urun1.Aciklama = "%5 indirim";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "kanepe";
+            urun2.Fiyati = 2.000;
+            urun2.Aciklama = "%10 indirim";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            int i = 0;
+            for (i = 0; i < urunler.Length; i++)
+
+            {
+                Console.WriteLine(urunler[i].Adi);
+            }
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+            }
+
+            
+            while (i < urunler.Length)
+            {
+
+                Console.WriteLine("ürünün adı :" + urunler[i].Adi);
+                Console.WriteLine("ürünün fiyatı:" + urunler[i].Fiyati);
+                Console.WriteLine("ürürnün açıklaması:" + urunler[i].Aciklama);
+                i++;
+            }
+
+        }
+
+    }
+}
+
---
+++ csart_odevlerim1/Urun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csart_odevlerim1
+{
+    class Urun
+    {
+
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+        public string Aciklama { get; set; }
+
+    }
+}
